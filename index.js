add6d77baccd52e773bb32f061dc01c722441d17
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-10-22 13:55:28 -0400

    Wrote questions to gather data and constructed shape object

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
-//* Started to write what i think i'll need, work in progress obviously
 // TODO: Write questions and figure out how to pass input for shape, color, and text to class constructor and make a new logo
 const inquirer = require('inquirer');
-const jest = require('jest');
 const fs = require('fs');
+const Shapes = require('./lib/shapes.js')
+const Triangle = require('./lib/shapes.js')
+const Circle = require('./lib/shapes.js')
+const Square = require('./lib/shapes.js')
 
-const question = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter up to three characters for the text of the logo:',
+        name: 'text'
+    },
+    {
+        type: 'input',
+        message: 'What color do you want to the text to be?',
+        name: 'textColor'
+    },
+    {
+        type: 'list',
+        message: 'Select a shape for your logo:',
+        choices: ['Triangle', 'Circle', 'Square'],
+        name: 'shape'
+    },
+    {
+        type: 'input',
+        message: 'What color do you want the shape?',
+        name: 'shapeColor'
+    }
+];
 
 function init() {
     inquirer
     .prompt(questions).then((data) => writeToFile(data));
 };
 
-function writeToFile(fileName, data) {
-    // let readMeContent = buildReadMe(data);
-    fs.writeFile('fileName', 'readMeContent', (err) => err ? console.error(err) : console.log('Generated logo'));
+function writeToFile(data) {
+
+    let newLogo = {};
+    const {text, textColor, shape, shapeColor} = data
+
+    if (shape == 'Triangle') {
+        newLogo = new Triangle(`${text}`, `${textColor}`, `${shapeColor}`);
+    } else if (shape == 'Circle') {
+        newLogo = new Circle(`${text}`, `${textColor}`, `${shapeColor}`)
+    } else if (shape == 'Square') {
+        newLogo = new Square(`${text}`, `${textColor}`, `${shapeColor}`)
+    }
+
+    console.log(newLogo);
+
+    // fs.writeFile(`./examples/${text}.svg`, newLogo, (err) => err ? console.error(err) : console.log('Generated logo'));
 }
 
 init();
